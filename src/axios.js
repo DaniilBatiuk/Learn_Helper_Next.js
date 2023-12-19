56334d7153802b86c57cde0ea22eb5eda181b316
DaniilBatiuk <sportak50@gmail.com>
2023-12-19 15:25:46 +0200

    update2

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+const instance = axios.create({
+  baseURL: "",
+});
+
 instance.interceptors.request.use(config => {
   config.headers.Authorization = window.localStorage.getItem("token");
 
