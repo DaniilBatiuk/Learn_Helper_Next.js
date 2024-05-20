d2bdd76b919dc6e030b9290f503a46265e2f5f47
Daniil Batiuk <sportak50@gmail.com>
2024-05-20 14:14:54 +0300

    fix text error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,14 @@
-import { TranslationForm } from "@/components/TranslateForm";
 import { ExampleList } from "@/components/ExampleList";
+import { TranslationForm } from "@/components/TranslateForm";
 import { TranslationsList } from "@/components/TranslationsList";
-import Head from "next/head";
 import { TranslationResult } from "@/interfaces/interface";
 import { useState } from "react";
 
 export default function Home() {
   const [translation, setTranslation] = useState<TranslationResult>({ translations: [], examples: [], synonyms: [] });
+
   return (
     <div className="home__container">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <TranslationForm setTranslation={setTranslation} translation={translation} />
       <TranslationsList translations={translation.translations} />
       <ExampleList examples={translation.examples} />
